976473789e1b70be098c5b4d33075e67e1d5b85d
Anetor <anetor.ataga@gmail.com>
2024-08-08 23:22:13 +0100

    Add project files.

---
+++ StudentCRUD/Model/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace StudentCRUD.Model
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options):  base(options) 
+        
+        {
+
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
---
+++ StudentCRUD/Model/Student.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StudentCRUD.Model
+{
+    public class Student
+    {
+        [Key]
+        public int StudentId { get; set; }
+
+        [Required]
+        [Column(TypeName = "navchar(250)")]
+        public string Name { get; set; } = "";
+
+        public string ContactNumber { get; set; } = "";
+        public int age { get; set; }
+    }
+}
